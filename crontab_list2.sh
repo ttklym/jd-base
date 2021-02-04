19aee33e306812d74639b609d2ae9160f1b70356
TTKLYM <shadowsocksr@yahoo.com>
2021-02-04 15:17:31 +0800

    删除Loon仓库脚本任务

--- crontab_list2.sh
+++ crontab_list2.sh
@@ -1,9 +1,3 @@
-# 宝洁美发屋
-1 8,9 14-31/1 1 * node /scripts/jd_bj.js >> /scripts/logs/jd_bj.log 2>&1
-
-# 源头好物红包
-0 0 * * *  node /scripts/jd_coupon.js >> /scripts/logs/jd_coupon.log 2>&1
-
 # 京东手机年终奖
 15 0 * * * node /scripts/jd_festival.js >> /scripts/logs/jd_festival.log 2>&1
 
